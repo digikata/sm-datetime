3a480dd8941fc44511d2b52584f7ced84f5814ec
Alan Chen <alan@infinyon.com>
2024-04-19 09:50:45 -0700

    add simple examples

--- justfile
+++ justfile
@@ -2,9 +2,23 @@
 default:
 	just -l
 
-test:
+code-test:
+	cargo test
+
+smdk-test:
 	smdk build
 	# smdk test --text '{ "foo":"2024/12/02 01:13:23" }'
-	smdk test --text "2024/12/02 01:13:23"
+	smdk test \
+		--text "2024/12/02 01:13:23" \
+		--params date_ops_params='{"source_format":"%Y/%d/%m %H:%M:%S","output_format":"%Y-%d-%m %H:%M:%S","source_timezone":2,"output_timezone":"UTC","fields":["foo"]}'
+
+# different source time zone
+smdk-test2:
+	smdk build
+	# smdk test --text '{ "foo":"2024/12/02 01:13:23" }'
+	smdk test \
+		--text "2024/12/02 01:13:23" \
+		--params date_ops_params='{"source_format":"%Y/%d/%m %H:%M:%S","output_format":"%Y-%d-%m %H:%M:%S","source_timezone":5,"output_timezone":"UTC","fields":["foo"]}'
+
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,13 @@ use serde::{Deserialize, Serialize};
 // use serde_json::{json, Value};
 
 use fluvio_smartmodule::{
-    dataplane::smartmodule::SmartModuleExtraParams, //, SmartModuleInitError},
+    dataplane::smartmodule::{SmartModuleExtraParams, SmartModuleInitError},
     eyre, smartmodule, SmartModuleRecord, RecordData, Result,
 };
 use chrono::{FixedOffset, NaiveDateTime, TimeZone};
 
 static SPEC: OnceCell<DateOpsParams> = OnceCell::new();
-// const PARAM_NAME: &str = "spec";
+static PARAM_NAME: &str = "date_ops_params";
 
 #[derive(Debug, Serialize, Deserialize)]
 struct DateOpsParams {
@@ -26,31 +26,12 @@ pub fn map(record: &SmartModuleRecord) -> Result<(Option<RecordData>, RecordData
     let key: Option<RecordData> = record.key.clone();
     let spec = SPEC.get().wrap_err("spec is not initialized")?;
     let date_str = record.value.as_str()?;
-    let formatted_date = date_change(&spec, date_str)?;
+    let formatted_date = match date_change(&spec, date_str) {
+        Ok(date) => date,
+        Err(e) => format!("error: {e}\n{date_str}")
+    };
 
     Ok((key, RecordData::from(formatted_date)))
-
-    // let record_data: Value = serde_json::from_slice(record.value.as_ref())?;
-
-    // let source_timezone = FixedOffset::east_opt(spec.source_timezone * 3600).unwrap();
-
-    // let mut updated_record_data = record_data.clone();
-    // let fields = vec!["foo"];
-    // let fiedls = spec.fields;
-    // for field in &fields {
-    //     if let Some(date_str) = record_data.get(field).and_then(Value::as_str) {
-    //         let ndt = NaiveDateTime::parse_from_str(date_str, &spec.source_format)
-    //             .map_err(|e| eyre!("Failed to parse date: {}", e))?;
-    //         // unwrap always succeeds because of Fixed offset of source_timezone
-    //         // see https://docs.rs/chrono/latest/chrono/offset/enum.MappedLocalTime.html
-    //         let dt_tz = source_timezone.from_local_datetime(&ndt).unwrap();
-    //         let utc_date = dt_tz.to_utc();
-    //         let formatted_date = utc_date.format(&spec.output_format).to_string();
-    //         updated_record_data[field] = json!(formatted_date);
-    //     }
-    // }
-
-    // Ok((key, RecordData::from(serde_json::to_vec(&updated_record_data)?)))
 }
 
 fn date_change(spec: &DateOpsParams, date_str: &str) -> Result<String> {
@@ -69,42 +50,48 @@ fn date_change(spec: &DateOpsParams, date_str: &str) -> Result<String> {
 fn t_date_change() -> Result<()> {
     let spec = DateOpsParams {
 		source_format: "%Y/%d/%m %H:%M:%S".to_string(),
-		output_format: "%Y-%d-%m %H:%M:%S'T'HH:MM:SS.SSS'Z'".to_string(),
-		source_timezone: 1,
+		output_format: "%Y-%d-%m %H:%M:%S".to_string(),
+		source_timezone: 2,
 		output_timezone: "UTC".to_string(),
         fields: vec!["foo".to_string()],
     };
-    let date = "2024/12/02 01:13:23";
-    let res = date_change(&spec, date)?;
-    dbg!(res);
+    let jspec = serde_json::to_string(&spec).expect("ser error");
+    println!("config: {jspec}");
+    let date_pre = "2024/12/02 01:13:23";
+    let date_post = date_change(&spec, date_pre)?;
+    dbg!(date_pre, date_post);
+
 
     Ok(())
 }
 
 #[smartmodule(init)]
-fn init(_params: SmartModuleExtraParams) -> Result<()> {
-    let spec = DateOpsParams {
-		source_format: "yyyy/dd/mm HH:MM:SS".to_string(),
-		output_format: "yyyy-mm-dd'T'HH:MM:SS.SSS'Z'".to_string(),
-		source_timezone: 0,
-		output_timezone: "UTC".to_string(),
-        fields: vec!["foo".to_string()],
-    };
-    SPEC.set(spec).expect("spec is already initialized");
-    Ok(())
-    // if let Some(raw_spec) = params.get(PARAM_NAME) {
-    //     match serde_json::from_str(raw_spec) {
-    //         Ok(spec) => {
-    //             SPEC.set(spec).expect("spec is already initialized");
-    //             Ok(())
-    //         }
-    //         Err(err) => {
-    //             eprintln!("unable to parse spec from params: {err:?}");
-    //             Err(eyre!("cannot parse `spec` param: {:#?}", err))
-    //         }
-    //     }
-    // } else {
-    //     Err(SmartModuleInitError::MissingParam(PARAM_NAME.to_string()).into())
-    // }
+fn init(params: SmartModuleExtraParams) -> Result<()> {
+
+    // hardcoded spec for testing
+    // let spec = DateOpsParams {
+	// 	source_format: "%Y/%d/%m %H:%M:%S".to_string(),
+	// 	output_format: "%Y-%d-%m %H:%M:%S".to_string(),
+	// 	source_timezone: 3,
+	// 	output_timezone: "UTC".to_string(),
+    //     fields: vec!["foo".to_string()],
+    // };
+    // SPEC.set(spec).expect("spec is already initialized");
+    // Ok(())
+
+    if let Some(raw_spec) = params.get(PARAM_NAME) {
+        match serde_json::from_str(raw_spec) {
+            Ok(spec) => {
+                SPEC.set(spec).expect("spec is already initialized");
+                Ok(())
+            }
+            Err(err) => {
+                eprintln!("unable to parse spec from params: {err:?}");
+                Err(eyre!("cannot parse `spec` param: {:#?}", err))
+            }
+        }
+    } else {
+        Err(SmartModuleInitError::MissingParam(PARAM_NAME.to_string()).into())
+    }
 }
 
